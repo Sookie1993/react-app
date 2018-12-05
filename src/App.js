ef198283f55c23d98c3209cdf262ac2450b827e6
Sookie1993 <1449521364@qq.com>
2018-12-05 21:21:30 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+// import { Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            测试
+          </p>
+          {/*<Button type="primary">Button</Button>*/}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
